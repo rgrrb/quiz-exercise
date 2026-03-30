0bc75be362dbdb0c009dfe288f29f0fb0828187e
Roger <cjo.rogerribeiro@gmail.com>
2026-03-30 08:17:45 -0300

    Create QuizGameScreenViewModel.kt

---
+++ app/src/main/java/com/example/quiz_exercise/screens/QuizGameScreenViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.quiz-exercise.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class QuizGameViewModel: ViewModel(){
+
+    private val _questionNumberState = MutableLiveData<Int>()
+    var questionNumber: LiveData<Int> = _questionNumberState
+
+}
